8b60b392d1acce3810e6a5d29c27e9bb7f0283c4
Eugenio Pace <eugenio.pace@gmail.com>
2012-11-28 18:22:02 -0800

    added account on prerender for user customization

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var mdoc = require('node-mdoc');
 var docsapp = new mdoc.App();
 
+docsapp.on('prerender', function( req, res, doc ){
+  res.locals.account = {};
+  res.locals.account.userName = 'Eugenio Pace';
+  res.locals.account.namespace =  'eugeniopace.auth0.com';
+  res.locals.account.clientId = 'ep-clientid';
+  res.locals.account.clientSecret = 'ep-clientsecret';
+
+  console.log(JSON.stringify(res.locals.account));
+}
+);
+
 if (!module.parent) {
   docsapp.start(3000);
   console.log('Server listening on port 3000');
