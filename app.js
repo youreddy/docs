4acdbfea04b56ef3edae7d7e53df432cb7ac3fc5
Matias Woloski <matiasw@gmail.com>
2012-12-04 09:32:48 -0300

    update to latest markdocs

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var mdoc = require('node-mdoc');
+var mdoc = require('markdocs');
 var docsapp = new mdoc.App();
 
 docsapp.on('prerender', function( req, res, doc ){
   res.locals.account = {};
   res.locals.account.userName = 'User Name';
-  res.locals.account.namespace =  'namespace.auth0.com';
-  res.locals.account.clientId = 'user-clientid';
-  res.locals.account.clientSecret = 'user-clientsecret';
+  res.locals.account.namespace =  'https://YOUR_NAMESPACE.auth0.com';
+  res.locals.account.clientId = 'YOUR_CLIENT_ID';
+  res.locals.account.clientSecret = 'YOUR_CLIENT_SECRET';
 
   console.log(JSON.stringify(res.locals.account));
 }
