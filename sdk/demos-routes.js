f2ef87aa9413d6082f70a181a05d310bedab5958
Matias Woloski <matiasw@gmail.com>
2013-07-31 19:47:01 -0300

    add css to widget selector and enhance others

--- sdk/demos-routes.js
+++ sdk/demos-routes.js
@@ -2,9 +2,10 @@ var widget_script_url = require('./widget_script_url');
 var nconf = require('nconf');
 
 var WIDGET_FALLBACK_CLIENTID = nconf.get('WIDGET_FALLBACK_CLIENTID');
+var DOMAIN_URL_DOCS = nconf.get('DOMAIN_URL_DOCS');
 
 module.exports = function (app) {
-  ['custom', 'embedded', 'link', 'login'].forEach(function (demo) {
+  ['custom', 'embedded', 'link', 'login', 'logincss'].forEach(function (demo) {
     app.get('/widget-demos/' + demo, function (req, res, next) {
       widget_script_url.get(req.query.a || WIDGET_FALLBACK_CLIENTID, function (err, url, tenant_domain) {
         if (err) return res.send(err);
@@ -12,6 +13,7 @@ module.exports = function (app) {
         res.locals.auth0_sdk_route = url;
         res.locals.tenant_domain   = tenant_domain;
         res.locals.clientID        = req.query.a;
+        res.locals.docs_route      = DOMAIN_URL_DOCS;
         
         next();
       });
