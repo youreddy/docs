1870ba87afcc1da1338aba16c0714a4aa94804b6
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-11-14 15:53:58 -0300

    moved ALL client-related operations to lib/clients.js

--- sdk/widget_script_url.js
+++ sdk/widget_script_url.js
@@ -1,5 +1,5 @@
 var nconf             = require('nconf');
-var getDb             = require('../lib/data');
+var clients           = require('../lib/clients');
 
 var env               = nconf.get('NODE_ENV');
 var cdn               = nconf.get('CDN');
@@ -8,25 +8,22 @@ var DOMAIN_URL_SDK    = nconf.get('DOMAIN_URL_SDK');
 var DOMAIN_URL_SERVER = nconf.get('DOMAIN_URL_SERVER');
 
 exports.get = function (clientID, done) {
-  getDb(function (db) {
-    db.collection('clients').findOne({clientID: clientID }, function (err, client) {
-        if (err) return done(err);
-        var tenant_domain = 'https://' + DOMAIN_URL_SERVER.replace('{tenant}', client ? client.tenant : 'YOUR-DOMAIN');
-        
-        var sdk_url;
+  clients.findByClientId(clientID, function (err, client) {
+    if (err) return done(err);
+    
+    var tenant_domain = 'https://' + DOMAIN_URL_SERVER.replace('{tenant}', client ? client.tenant : 'YOUR-DOMAIN');
+    var sdk_url;
 
-        if (env === 'production' && !disable_cdn) {
-          if (cdn) {
-            sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID + '&cdn=https://' + cdn;
-          } else {
-            sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID;
-          }
-        } else {
-          sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID + '&cdn=' + tenant_domain + '&assets=' + tenant_domain;
-        }
+    if (env === 'production' && !disable_cdn) {
+      if (cdn) {
+        sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID + '&cdn=https://' + cdn;
+      } else {
+        sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID;
+      }
+    } else {
+      sdk_url = 'https://' + DOMAIN_URL_SDK + '/auth0.js#client=' + clientID + '&cdn=' + tenant_domain + '&assets=' + tenant_domain;
+    }
 
-        done(null, sdk_url, tenant_domain, client);
-
-      });
-    });
+    done(null, sdk_url, tenant_domain, client);
+  });
 };
