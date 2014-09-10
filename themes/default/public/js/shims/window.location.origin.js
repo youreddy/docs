576ccdb7934dfc43c916b2e43c9ef3eb64570449
Cristian Douce <cristian@gravityonmars.com>
2014-09-10 11:50:06 -0300

    Move shims to base.jade for theme and update index.md to cleanup

---
+++ themes/default/public/js/shims/window.location.origin.js
@@ -0,0 +1,4 @@
+// IE8/9 SHIM
+if (!window.location.origin) {
+  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+}
